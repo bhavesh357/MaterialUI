d24ab8cf6412fac1b20da4223f4d7a17846da54f
bhavesh357 <bkadam357@gmail.com>
2020-07-20 02:17:38 +0530

    REFACTORED: to style tile header

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -7,8 +7,9 @@ class Tile extends React.Component{
             <div className="tile">
                 <div className="tile-title">{this.props.tile.plan}</div>
                 <div className="tile-tagline">{this.props.tile.tagLine? this.props.tile.tagLine : ""}</div>
-                {this.props.tile.tagLine? <div className="tile-tagline">{this.props.tile.plan}</div> : <div className="tile-tagline"></div>}
-                <div className="tile-price">{this.props.tile.price}</div>
+                <div className="tile-price">
+                    <div className="price">${this.props.tile.price}</div>/mo
+                </div>
                 <div className="tile-users">{this.props.tile.users}</div>
                 <div className="tile-storage">{this.props.tile.storage}</div>
                 <div className="tile-help">{this.props.tile.help}</div>
