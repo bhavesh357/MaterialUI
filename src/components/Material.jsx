dafe7f0ab13457c5f02c304d3986b6ce6dc16385
bhavesh357 <bkadam357@gmail.com>
2020-07-21 01:33:28 +0530

    ADDED: router to the project

--- src/components/Material.jsx
+++ src/components/Material.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import Header from './Header';
 import Content from './Content';
-import Footer from './Footer'
+import Footer from './Footer';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 class Material extends React.Component{
     render() {
         return (
+            <Router>
             <div id="container">
             <Header navbarButtons={this.props.navbarButtons}/>
-            <Content 
-                title="Pricing" 
-                desc="Quickly build an effective pricing table for your potential customers with this layout. It's built with default Material-UI components with little customization."
-                tiles={this.props.tiles}
-            />
+            <Switch>
+                <Route path="/" exact render={() => <Content 
+                        title="Pricing" 
+                        desc="Quickly build an effective pricing table for your potential customers with this layout. It's built with default Material-UI components with little customization."
+                        tiles={this.props.tiles}
+                    />}>
+                </Route>
+            </Switch>
+            
             <Footer footerMenu={this.props.footerMenu}/>
             </div>
-        );
+            </Router>
+            );
+        }
     }
-}
-
-export default Material;
+    
+    export default Material;
